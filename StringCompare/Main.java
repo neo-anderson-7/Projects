d9cbeb9d6a613f4035827ea4b694348aa0acaf1d
neo-anderson-7 <haydengunk@gmail.com>
2020-03-14 10:06:15 +0530

    Switch replaced by if

--- StringCompare/Main.java
+++ StringCompare/Main.java
@@ -3,37 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // boolean again = true;
+        boolean again = false;
 
-        // do {
+        do {
             System.out.println(stringComapare(scan));
-            // System.out.print("Do you want to compare again (y/n)? ");
-            // String ans = scan.next();
-            // again = (ans == "y") ? true : false;
-        // } while (again);
+            System.out.print("Do you want to compare again (y/n)? ");
+            String ans = scan.next();
+            if (ans == "y") {
+                again = true;
+            } else if (ans == "n") {
+                again = false;
+            } else {
+                System.out.print(ans);
+                System.out.print("Wrong Input! Exiting program.");
+            }
+        } while (again);
 
         scan.close();
     }
 
-    public static String stringComapare(Scanner scan) {     // Error is due to this method
+    public static String stringComapare(Scanner scan) {
         System.out.print("Enter first string: ");
         String a = scan.next();
         System.out.print("Enter second string: ");
         String b = scan.next();
 
         int key = a.compareToIgnoreCase(b);
-        switch (key) {
-            case 0:
-                return("Both strings are same");
-            
-            case -1:
-                return(a + " comes before " + b);
-            
-            case 1:
-                return(a + " comes before " + b);
-            
-            default:
-                return("Error!");
+        if (key == 0) {
+            return("Both strings are same");
+        } else if (key > 0) {
+            return(a + " comes before " + b);
+        } else if (key < 0) {
+            return(a + " comes before " + b);
+        } else {
+            return("Error!");
         }
     }
 }
