b1bdd6412ebef0e39fd74f0c64081b209bc2df8b
neo-anderson-7 <haydengunk@gmail.com>
2020-03-14 20:41:16 +0530

    Correct skeleton

--- StringCompare/Main.java
+++ StringCompare/Main.java
@@ -3,25 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        boolean again = false;
-
-        do {
-            System.out.println(stringComapare(scan));
-            System.out.print("Do you want to compare again (y/n)? ");
-            String ans = scan.next();
-            if (ans == "y") {
-                again = true;
-            } else if (ans == "n") {
-                again = false;
-            } else {
-                System.out.print("Wrong Input! Exiting program.");
-            }
-        } while (again);
-
-        scan.close();
-    }
-
-    public static String stringComapare(Scanner scan) {
         System.out.print("Enter first string: ");
         String a = scan.next();
         System.out.print("Enter second string: ");
@@ -29,13 +10,15 @@ public class Main {
 
         int key = a.compareToIgnoreCase(b);
         if (key == 0) {
-            return("Both strings are same");
-        } else if (key > 0) {
-            return(a + " comes before " + b);
+            System.out.print("Both strings are equal");
         } else if (key < 0) {
-            return(a + " comes before " + b);
+            System.out.print(a + " comes before " + b);
+        } else if (key > 0) {
+            System.out.print(b + " comes before " + a);
         } else {
-            return("Error!");
+            System.out.print("Error");
         }
+
+        scan.close();
     }
 }
