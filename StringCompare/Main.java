a133a9e483141568fe73a9d05eaaedc583a7eb58
neo-anderson-7 <haydengunk@gmail.com>
2020-03-14 20:52:07 +0530

    Function added

--- StringCompare/Main.java
+++ StringCompare/Main.java
@@ -4,21 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter first string: ");
-        String a = scan.next();
+        String a = scan.nextLine();
         System.out.print("Enter second string: ");
-        String b = scan.next();
-
-        int key = a.compareToIgnoreCase(b);
-        if (key == 0) {
-            System.out.print("Both strings are equal");
-        } else if (key < 0) {
-            System.out.print(a + " comes before " + b);
-        } else if (key > 0) {
-            System.out.print(b + " comes before " + a);
-        } else {
-            System.out.print("Error");
-        }
+        String b = scan.nextLine();
+        System.out.print(stringCompare(a, b));
 
         scan.close();
+    }
+
+    public static String stringCompare(String a, String b) {
+        int key = a.compareToIgnoreCase(b);
+        if (key == 0) {
+            return("Both strings are equal");
+        } else if (key < 0) {
+            return(a + " comes before " + b);
+        } else if (key > 0) {
+            return(b + " comes before " + a);
+        } else {
+            return("Error");
+        }
     }
 }
